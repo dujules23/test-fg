de74ea01bfd03a0d47f60615d221b50f4765673a
Durrell Jules <dujules23@gmail.com>
2023-10-05 15:03:56 -0400

    set up basic environment with bbackground image in the game loop, renders succesfully

--- main.py
+++ main.py
@@ -7,17 +7,35 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 pygame.display.set_caption("Brawler")
+
+# load background image
+bg_image = pygame.image.load(
+    "assets/images/background/background.jpg").convert_alpha()
+
+# function for drawing background
+
+
+def draw_bg():
+    # bg has to be scaled to fit the window size
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
 
 # game loop
 run = True
 while run:
 
+    # draw background
+    draw_bg()
+
+    # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    # update display
+    pygame.display.update()
 
 # exit pygame
 pygame.quit()
